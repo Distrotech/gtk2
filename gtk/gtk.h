779384ac151e79647e0b597fd48a960b2a9264a9
Carlos Garnacho <carlos@lanedo.com>
2013-01-17 21:08:20 +0100

    Add GtkGestureZoom
    This gesture interprets and reports relative scale differences when fed
    with events from two different GdkEventSequences.

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -112,6 +112,7 @@
 #include <gtk/gtkgesturelongpress.h>
 #include <gtk/gtkgesturerotate.h>
 #include <gtk/gtkgestureswipe.h>
+#include <gtk/gtkgesturezoom.h>
 #include <gtk/gtkgrid.h>
 #include <gtk/gtkiconfactory.h>
 #include <gtk/gtkicontheme.h>
